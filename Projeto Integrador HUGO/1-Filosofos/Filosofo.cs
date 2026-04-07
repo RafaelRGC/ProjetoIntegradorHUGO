e4ed35b61441a6302bc7828798d31b9b1d807334
Rafael Gonçalves <rafaelgoncalvescastro0@gmail.com>
2026-04-07 10:48:52 -0300

    Add files via upload

---
+++ Projeto Integrador HUGO/1-Filosofos/Filosofo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Filosofo
+{
+    public string Nome;
+    private Talheres talheres;
+
+    public Filosofo(string nome, Talheres t)
+    {
+        Nome = nome;
+        talheres = t;
+    }
+
+    public void Pensar()
+    {
+        Console.WriteLine(Nome + " está pensando ");
+    }
+
+    public void PegarFaca()
+    {
+        if (talheres.FacaLivre)
+        {
+            talheres.FacaLivre = false;
+            talheres.DonoFaca = Nome;
+            Console.WriteLine(Nome + " pegou a faca (α)");
+        }
+        else
+        {
+            Console.WriteLine("Não pode pegar a faca, está com " + talheres.DonoFaca);
+        }
+    }
+
+    public void PegarGarfo()
+    {
+        if (talheres.GarfoLivre)
+        {
+            talheres.GarfoLivre = false;
+            talheres.DonoGarfo = Nome;
+            Console.WriteLine(Nome + " pegou o garfo (β)");
+        }
+        else
+        {
+            Console.WriteLine("Não pode pegar o garfo, está com " + talheres.DonoGarfo);
+        }
+    }
+
+    public void Comer()
+    {
+        if (talheres.DonoFaca == Nome && talheres.DonoGarfo == Nome)
+        {
+            Console.WriteLine(Nome + " está comendo ");
+        }
+        else
+        {
+            Console.WriteLine(Nome + " não pode comer, faltam talheres");
+        }
+    }
+
+    public void DevolverTalheres()
+    {
+        if (talheres.DonoFaca == Nome)
+        {
+            talheres.FacaLivre = true;
+            talheres.DonoFaca = "";
+        }
+
+        if (talheres.DonoGarfo == Nome)
+        {
+            talheres.GarfoLivre = true;
+            talheres.DonoGarfo = "";
+        }
+
+        Console.WriteLine(Nome + " devolveu os talheres ");
+    }
+}
---
+++ Projeto Integrador HUGO/1-Filosofos/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class Program
+{
+    static void MenuFilosofo(Filosofo f)
+    {
+        int opcao;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("MENU " + f.Nome);
+            Console.WriteLine("1 - Pensar");
+            Console.WriteLine("2 - Pegar Faca");
+            Console.WriteLine("3 - Pegar Garfo");
+            Console.WriteLine("4 - Comer");
+            Console.WriteLine("5 - Devolver Talheres");
+            Console.WriteLine("0 - Voltar");
+
+            Console.Write("\nEscolha: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            switch (opcao)
+            {
+                case 1:
+                    f.Pensar();
+                    break;
+
+                case 2:
+                    f.PegarFaca();
+                    break;
+
+                case 3:
+                    f.PegarGarfo();
+                    break;
+
+                case 4:
+                    f.Comer();
+                    break;
+
+                case 5:
+                    f.DevolverTalheres();
+                    break;
+            }
+
+            if (opcao != 0)
+            {
+                Console.WriteLine("\nPressione qualquer tecla...");
+                Console.ReadKey();
+            }
+
+        } while (opcao != 0);
+    }
+
+    static void Main()
+    {
+        Talheres talheres = new Talheres();
+
+        Filosofo F1 = new Filosofo("F1", talheres);
+        Filosofo F2 = new Filosofo("F2", talheres);
+
+        int opcao;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("CONTROLE DOS FILOSOFOS\n");
+
+            Console.WriteLine("1 - Filosofo F1");
+            Console.WriteLine("2 - Filosofo F2");
+            Console.WriteLine("0 - Sair");
+
+            Console.Write("\nEscolha: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    MenuFilosofo(F1);
+                    break;
+
+                case 2:
+                    MenuFilosofo(F2);
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
+}
